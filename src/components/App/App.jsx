4a0680e1cc952048c469f3bb3f9db3e639629422
Zoe Lindman <zoe.lindman@gmail.com>
2024-04-28 21:21:36 -0500

    restructured app into one component instead of two, adjusted functions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,66 @@
 import './App.css';
-import ToDoList from './ToDoList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App () {
   
   const [toDoItem, setToDoItem] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const getTaskList = () => {
+    axios.get('/api/todo').then(response => {
+      setToDoItem(response.data);
+    }).catch(error => {
+      console.log(error);
+      alert('Something went wrong!');
+    })
+  }
+
+  useEffect(() => {
+    console.log('useEffect!');
+    getTaskList();
+  }, []);
+
+
+  function addTask(){
+      setTasks
+  }
+
+  function deleteTask(id){
+
+  }
+
+  function prioritizeTask(id){
+
+  }
+
 
   return (
     <div>
       <h1>TO DO APP</h1>
-      <ToDoList/>
+      <div className="to-do-list">
+        <form onSubmit={addTask}>
+          <input
+            type="text"
+            placeholder="Enter a task..."
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <input 
+          type="submit" value="Add Task"
+          /> 
+        </form>
+        <ol>
+            {tasks.map((task, id) => 
+                <li key={id}>
+                    {task}
+                    <button onClick={() => deleteTask(id)}>Delete</button>
+                    <button onClick={() => prioritizeTask(id)}>Priority</button>
+                </li>
+        )}
+        </ol>
+      </div>
     </div>
   );
 
